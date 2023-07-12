df6fac487a4074c21f8af30fa9e21db63990ead1
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-07-12 08:39:44 +0530

    Link in next

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const about = () => {
   return (
-    <div>about</div>
-  )
-}
+    <div>
+      <ul>
+        <li>
+      <Link href="/about/1">Yash</Link>
 
-export default about
+        </li>
+        <li>
+      <Link href="/about/2">Vaibhav</Link>
+
+        </li>
+        <li>
+      <Link href="/about/3">Suresh</Link>
+
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default about;
